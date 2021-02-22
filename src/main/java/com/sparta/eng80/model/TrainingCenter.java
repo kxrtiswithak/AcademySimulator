c06edb261a14f7124d0341caad39b0fa6d6289be
bsrwilliams <bradleysrwilliams@outlook.com>
2021-02-22 22:09:25 +0000

    Added adding trainee and training center creation functionality

---
+++ src/main/java/com/sparta/eng80/model/TrainingCenter.java
@@ -0,0 +1,27 @@
+package com.sparta.eng80.model;
+
+import java.util.List;
+
+public class TrainingCenter {
+    //private static int ID;
+    private String name;
+    private int size;
+    private List<Trainee> inTraining;
+
+    public TrainingCenter(String name) {
+        this.name = name;
+        this.size = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public List<Trainee> getInTraining() {
+        return inTraining;
+    }
+}
