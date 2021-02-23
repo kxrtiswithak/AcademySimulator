a3a9646117e637963f2011296990d2c0f029ffe4
Mysters789 <mysters789@gmail.com>
2021-02-23 11:14:19 +0000

    Added test cases for Trainee and TrainingCenter class. Added a addTrainee Method that added validation for the number of trainees to be no more then 100.

--- src/main/java/com/sparta/eng80/model/TrainingCenter.java
+++ src/main/java/com/sparta/eng80/model/TrainingCenter.java
@@ -1,16 +1,18 @@
 package com.sparta.eng80.model;
 
+import com.sparta.eng80.view.Printer;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class TrainingCenter {
     //private static int ID;
     private String name;
-    private int size;
     private List<Trainee> inTraining;
 
     public TrainingCenter(String name) {
         this.name = name;
-        this.size = 0;
+        this.inTraining = new ArrayList<>();
     }
 
     public String getName() {
@@ -18,10 +20,25 @@ public class TrainingCenter {
     }
 
     public int getSize() {
-        return size;
+        return getInTraining().size();
     }
 
     public List<Trainee> getInTraining() {
         return inTraining;
+    }
+
+    public boolean addToTrainingCentre(Trainee trainee) {
+        if (trainee == null) {
+            Printer.printString("Null trainee cannot be added, sorry");
+            return false;
+        } else if (inTraining.contains(trainee)) {
+            Printer.printString("Trainee already exists, so wont be added");
+            return false;
+        } else if (getSize() == 100) {
+            Printer.printString("Training Centre is full, please add to a different one.");
+            return false;
+        }
+        inTraining.add(trainee);
+        return true;
     }
 }
