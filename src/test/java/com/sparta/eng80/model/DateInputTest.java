bea942e21dc61b5ae59bfb26cd60feb06e362294
WahdelMW <WWoodhouse@spartaglobal.com>
2021-02-23 16:20:47 +0000

    Added Tests fir random generator

--- src/test/java/com/sparta/eng80/model/DateInputTest.java
+++ src/test/java/com/sparta/eng80/model/DateInputTest.java
@@ -1,6 +1,6 @@
 package com.sparta.eng80.model;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class DateInputTest {
     @Test
--- src/test/java/com/sparta/eng80/model/TraineeManagerTest.java
+++ src/test/java/com/sparta/eng80/model/TraineeManagerTest.java
@@ -1,30 +1,34 @@
 package com.sparta.eng80.model;
 
 import com.sparta.eng80.view.Printer;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class TraineeManagerTest {
+    final int MIN = 20;
+    final int MAX = 30;
+
     @Test
     public void generateBetween20And30Trainees() {
         TraineeManager traineeManager = new TraineeManager();
-        int min = 20;
-        int max = 30;
-        List<List<Trainee>> listOflistsOfTrainees = new ArrayList<>();
+        List<List<Trainee>> listOfListsOfTrainees = new ArrayList<>();
         for (int count = 0; count < 5; count++) {
-            List<Trainee> listOfTrainees = traineeManager.generateNewTrainees(min, max);
-            listOflistsOfTrainees.add(listOfTrainees);
+            List<Trainee> listOfTrainees = traineeManager.generateNewTrainees(MIN, MAX);
+            listOfListsOfTrainees.add(listOfTrainees);
         }
-        for (List<Trainee> i : listOflistsOfTrainees) {
+        for (List<Trainee> i : listOfListsOfTrainees) {
             Assertions.assertTrue(i.size() >= 20 && i.size() <= 30);
         }
     }
 
     @Test
-    public void Test2() {
-
+    public void generateUniqueTraineeLists() {
+        TraineeManager traineeManager = new TraineeManager();
+        List<Trainee> traineeList1 = traineeManager.generateNewTrainees(MIN, MAX);
+        List<Trainee> traineeList2 = traineeManager.generateNewTrainees(MIN, MAX);
+        Assertions.assertFalse(traineeList1.equals(traineeList2));
     }
 }
