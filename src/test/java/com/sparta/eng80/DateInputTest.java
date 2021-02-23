6681ad1d86787eebab266dcbeda02e48b5f58562
WahdelMW <WWoodhouse@spartaglobal.com>
2021-02-23 14:13:17 +0000

    Updated Tests for date input

--- src/test/java/com/sparta/eng80/DateInputTest.java
+++ src/test/java/com/sparta/eng80/DateInputTest.java
@@ -1,24 +1,63 @@
 package com.sparta.eng80;
 
 import com.sparta.eng80.model.Simulation;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class DateInputTest {
+    Simulation simulation;
+    @BeforeEach
+    public void setup() {
+    simulation = new Simulation();
+    }
+
+    @Test
+    public void inputNotValidMonth() {
+        simulation.setSimulationFor(0);
+        simulation.run();
+    }
+
+    @Test
+    public void inputNotValidMonthAndYear() {
+        simulation.setSimulationFor(1,3);
+        simulation.run();
+    }
+
+    @Test
+    public void inputNotValidMonthYearAndDay() {
+        simulation.setSimulationFor(2, 12, 19);
+        simulation.run();
+    }
+
+    @Test
+    public void inputDateBeforeCurrent() {
+        simulation.setSimulationUntil(2020, 01,01);
+        simulation.run();
+    }
+
     @Test
     public void inputValidMonth() {
-        Simulation simulation = new Simulation();
-        simulation.setSimulationFor(5);
+        simulation.setSimulationFor(0);
+        simulation.run();
     }
 
     @Test
     public void inputValidMonthAndYear() {
-        Simulation simulation = new Simulation();
         simulation.setSimulationFor(1,3);
+        simulation.run();
     }
 
     @Test
     public void inputValidMonthYearAndDay() {
-        Simulation simulation = new Simulation();
         simulation.setSimulationFor(2, 12, 19);
+        simulation.run();
+    }
+
+    @Test
+    public void inputDateAfterCurrent() {
+        simulation.setSimulationUntil(2020, 01,01);
+        simulation.run();
     }
 }
