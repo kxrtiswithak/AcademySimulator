faaad74e405bc95a66e92f669f791f936e1589d5
Joel Fright <joelwfright@gmail.com>
2021-02-24 15:04:37 +0000

    Added bootcamp and tech centre classes and set max sizes

---
+++ src/main/java/com/sparta/eng80/model/types_of_centres/Bootcamp.java
@@ -0,0 +1,13 @@
+package com.sparta.eng80.model.types_of_centres;
+
+import com.sparta.eng80.model.TrainingCentre;
+
+public class Bootcamp extends TrainingCentre {
+
+    public Bootcamp(String name) {
+        super(name);
+        this.MAX_SIZE = 500;
+    }
+
+
+}
