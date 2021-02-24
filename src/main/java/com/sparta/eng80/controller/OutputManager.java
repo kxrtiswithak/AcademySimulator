a0ebba1e934622e56dac382aaa6392a9a357341f
bsrwilliams <bradleysrwilliams@outlook.com>
2021-02-24 13:39:55 +0000

    Added user input functionality

--- src/main/java/com/sparta/eng80/controller/OutputManager.java
+++ src/main/java/com/sparta/eng80/controller/OutputManager.java
@@ -14,17 +14,22 @@ public class OutputManager implements Output {
     TraineeManager traineeManager;
     LocalDate endingDate;
     FileOutput fileOutput;
+    boolean outputToFile;
 
-    public OutputManager(TrainingCentreManager trainingCentreManager, TraineeManager traineeManager, LocalDate endingDate) {
+    public OutputManager(TrainingCentreManager trainingCentreManager, TraineeManager traineeManager, LocalDate endingDate, boolean outputToFile) {
         this.trainingCentreManager = trainingCentreManager;
         this.traineeManager = traineeManager;
         this.endingDate = endingDate;
         this.fileOutput = new FileOutput(outputNumOfOpenCentres(), outputNumOfFullCentres(), outputNumOfTraineesInTraining(), outputNumOfTraineesInWaitingList(), overallProjectTime(endingDate));
+        this.outputToFile = outputToFile;
     }
 
     public void run() {
-        fileOutput.outputToFile();
-        fileOutput.outputToConsole();
+        if (this.outputToFile) {
+            fileOutput.outputToFile();
+        } else {
+            fileOutput.outputToConsole();
+        }
     }
 
     @Override
--- src/main/java/com/sparta/eng80/start/Main.java
+++ src/main/java/com/sparta/eng80/start/Main.java
@@ -1,5 +1,6 @@
 package com.sparta.eng80.start;
 
+import com.sparta.eng80.App;
 import com.sparta.eng80.view.Starter;
 
 public class Main {
