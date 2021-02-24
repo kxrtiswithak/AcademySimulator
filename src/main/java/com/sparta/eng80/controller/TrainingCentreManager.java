51b9a53bea43bf7125eef1952e840d7caf110185
Joel Fright <joelwfright@gmail.com>
2021-02-24 15:25:22 +0000

    Added generation for bootcamp and tech centre

--- src/main/java/com/sparta/eng80/controller/TrainingCentreManager.java
+++ src/main/java/com/sparta/eng80/controller/TrainingCentreManager.java
@@ -1,15 +1,21 @@
 package com.sparta.eng80.controller;
 
 import com.sparta.eng80.model.TrainingCentre;
+import com.sparta.eng80.model.types_of_centres.Bootcamp;
+import com.sparta.eng80.model.types_of_centres.TechCentre;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class TrainingCentreManager {
     private int numberOfTrainingCentres = 0;
+    private int numberOfBootcamps = 0;
+    private int numberOfTechCentres = 0;
     private LocalDate startDate;
     private LocalDate currentDate;
     private LocalDate lastCentreAddedDate;
+    private LocalDate lastBootcampAddedDate;
+    private LocalDate lastTechCentreAddedDate;
     private ArrayList<TrainingCentre> listOfTrainingCentres = new ArrayList<>();
 
     public TrainingCentreManager(LocalDate startDate) {
@@ -19,13 +25,31 @@ public class TrainingCentreManager {
     public void generateNewCentre(LocalDate currentDate) {
         String trainingCentreName = "Training Center " + numberOfTrainingCentres;
         this.currentDate = currentDate;
-        //        Printer.printString("Current: " + currentDate + " last: " + lastCentreAddedDate);
         if (currentDate.equals(lastCentreAddedDate.plusMonths(2)) || currentDate.equals(startDate)) {
             TrainingCentre trainingCentre = new TrainingCentre(trainingCentreName);
             numberOfTrainingCentres++;
             lastCentreAddedDate = currentDate;
             listOfTrainingCentres.add(trainingCentre);
-            //            Printer.printString("New Training Center added: " + trainingCenter.getName() + " On " + currentDate);
+        }
+    }
+
+    public void generateNewBootcamp(LocalDate currentDate){
+        String bootcampName = "Boot Camp " + numberOfBootcamps;
+        if(numberOfBootcamps < 2 || currentDate.equals(lastBootcampAddedDate.plusMonths(3)) || currentDate.equals(startDate)){
+            Bootcamp bootcamp = new Bootcamp(bootcampName);
+            numberOfBootcamps++;
+            lastBootcampAddedDate = currentDate;
+            listOfTrainingCentres.add(bootcamp);
+        }
+    }
+
+    public void generateNewTechCentre(LocalDate currentDate){
+        String techCentreName = "Tech Centre " + numberOfTechCentres;
+        if(currentDate.equals(lastTechCentreAddedDate.plusMonths(2)) || currentDate.equals(startDate)){
+            TechCentre techCentre = new TechCentre(techCentreName);
+            numberOfTechCentres++;
+            lastTechCentreAddedDate = currentDate;
+            listOfTrainingCentres.add(techCentre);
         }
     }
 
